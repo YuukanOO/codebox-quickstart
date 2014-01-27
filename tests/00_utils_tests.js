d9f708eaa866bf89876e071528b69615a7d94078
Julien LEICHER <jleicher@gmail.com>
2014-01-27 15:30:44 +0100

    Updated tests + fixes

--- tests/00_utils_tests.js
+++ tests/00_utils_tests.js
@@ -28,16 +28,36 @@ exports.testGetFilesSync = function(test) {
 
     var filepath_a = path_a + "/filea.txt";
     var filepath_b = path_b + "/fileb.txt";
+    var filepath_c = path_b + "/exclude.me";
+    var filepath_d = path_b + "/exclude.metoo";
 
     mkdirp.sync(path_b);
 
+    // Creates some dummy files
     fs.writeFileSync(filepath_a, "Some content in a file.");
     test.ok(fs.existsSync(filepath_a));
 
     fs.writeFileSync(filepath_b, "Some content in b file.");
     test.ok(fs.existsSync(filepath_b));
 
+    fs.writeFileSync(filepath_c, "Dummy content");
+    test.ok(fs.existsSync(filepath_c));
+
+    fs.writeFileSync(filepath_d, "Dummy content");
+    test.ok(fs.existsSync(filepath_d));
+
+    // First without exclude filters
     var results = utils.getFilesSync(root_path);
+
+    test.equals(results.length, 4);
+
+    // And then with exclude filters
+    var to_exclude = [
+        new RegExp(".metoo$"),
+        new RegExp(".me$")
+    ];
+
+    results = utils.getFilesSync(root_path, to_exclude);
 
     test.equals(results.length, 2);
 
