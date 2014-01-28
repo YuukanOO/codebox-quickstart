0639ea65ff9264c7acb2bb88f94608527e585d23
Julien LEICHER <jleicher@gmail.com>
2014-01-28 14:52:55 +0100

    Added option -c <services,...>

--- bin/codebox-quickstart.js
+++ bin/codebox-quickstart.js
@@ -9,11 +9,27 @@ var unit        = require('../lib/unit');
 var addon_path  = null;
 var view        = {};
 
+function list(val) {
+    var parts = val.split(',');
+    var res = [];
+
+    for(var i = 0; i < parts.length; i++) {
+        res.push({
+            name: parts[i],
+            index: i,
+            str: '"' + parts[i] + '"' + ((i == parts.length - 1) ? '' : ',')
+        });
+    }
+
+    return res;
+}
+
 program
     .version(pkg.version)
     .usage('[options] <addon_path>')
-    .option('-s, --serverside', 'Creates needed files for server side addon.')
-    .option('-e, --expert', 'Expert mode, no comment will be added to the generated code.')
+    .option('-s, --serverside', 'Creates needed files for server side addon')
+    .option('-c, --consumes <services>', 'Consumes comma separated list of service', list)
+    .option('-e, --expert', 'Expert mode, no comment will be added to the generated code')
     //.option('-i, --interactive', 'Interactive mode, prompt the user.')
     .parse(process.argv);
 
@@ -26,15 +42,20 @@ if(addon_path === null/* && !program.interactive*/) {
 }
 
 // Construct the view needed by mustache
+var consumer = typeof(program.consumes) !== 'undefined';
+
 view.gen = {
-    'server-side': program.serverside,
-    'rookie-mode': !program.expert
+    'server-side': program.serverside || consumer,
+    'rookie-mode': !program.expert,
+    'consumer': consumer
 };
 
 view.addon = {
     'name': path.basename(addon_path),
     'version': "0.1.0",
-    'path': addon_path
+    'path': addon_path,
+    'consumes': program.consumes,
+
 };
 
 view.author = {
